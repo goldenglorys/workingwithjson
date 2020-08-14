4140765320bc4b05b700ef85f45d87fc069d2b7c
Olusola Glory <olusolagloryolamide@gmail.com>
2020-08-14 16:09:22 +0100

    code reformatted and a function formatting a json given file to csv file implemented...

--- input.py
+++ input.py
@@ -4,10 +4,51 @@
 """
 
 import json
+from random import randint
 
-with open('file.json', 'r') as o:
-    file_object = json.load(o)
-    with open('output.txt', 'w') as output:
-        for person in file_object:
-            output.write(person['first_name'] + ' ' + person['last_name'] + '\n')
-print('Done')
+
+def collate_json_values_to_file():
+    with open('file.json', 'r') as o:
+        file_object = json.load(o)
+        with open('output.txt', 'w') as output:
+            for person in file_object:
+                output.write(person['first_name'] + ' ' + person['last_name'] + '\n')
+    print('Done')
+
+
+write_heading = False
+csv_heading = []
+outputfilename = str(randint(0, 100000)) + 'result.csv'
+
+
+def convert_json_to_csv():
+    global write_heading, outputfilename
+    json_file = input('Enter JSON file name to convert: ')
+    try:
+        print("====================================================================================")
+        print("Program Starting...")
+        with open(str(json_file), 'r') as o:
+            file_object = json.load(o)
+            print("Json file given loaded...")
+            with open(outputfilename, 'a') as result:
+                print("Output csv file created...")
+                if not write_heading:
+                    for head in file_object[0].keys():
+                        result.write(head + ',')
+                        csv_heading.append(str(head))
+                    print("CSV heading title appended to the output file...")
+                    write_heading = True
+
+                print("Started dumping json values to the CSV file...")
+                for obj in file_object:
+                    result.write('\n')
+                    for csv_head in csv_heading:
+                        result.write(str(obj[csv_head]) + ',')
+                    # result.write('\n' + str(obj['user_id']) + ','+ obj['first_name'] + ',' + obj['last_name'])
+                print('Program completed successfully! with file name of' + outputfilename)
+
+    except FileNotFoundError:
+        print("File not found program exit!")
+
+
+convert_json_to_csv()
